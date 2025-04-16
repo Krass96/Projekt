93f083a17397154cbe0e30d9e16cfc69a17ae95d
Krass96 <gyncoo@abv.bg>
2025-04-16 14:11:00 +0200

    added main by artist_galery

--- artemi_project/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:artemi_project/src/features/artist_galery/my_app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
---
+++ artemi_project/lib/src/features/artist_galery/main.dart
@@ -0,0 +1,6 @@
+import 'package:artemi_project/src/features/artist_galery/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
