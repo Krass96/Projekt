5827e9330bb8f02921c06f0443e8533881c54ff0
Krass96 <gyncoo@abv.bg>
2025-05-05 10:13:37 +0200

    updated App name

--- artemi_project/lib/main.dart
+++ artemi_project/lib/main.dart
@@ -2,10 +2,11 @@ import 'package:artemi_project/src/features/artemi/Screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
